8b33512c5fca7c43896ba972e64c7505c55b4882
doodin <mongji09@naver.com>
2025-04-22 01:28:58 +0900

    충돌 파일 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8c157eb9b616b8085218ea0e34c063dfe1678fc8
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import globals from 'globals';
-import someConfig from 'some-other-config-you-use';
-import eslintConfigPrettier from 'eslint-config-prettier';
-<<<<<<< HEAD
-=======
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
->>>>>>> 8e2e46c (충돌 해결)
-=======
->>>>>>> 8c157eb9b616b8085218ea0e34c063dfe1678fc8
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -26,14 +12,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8c157eb9b616b8085218ea0e34c063dfe1678fc8
   js.configs.recommended,
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  someConfig,
-  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
@@ -42,17 +22,10 @@ const eslintConfig = [
       },
     },
     rules: {
-      // 여기에 추가적인 규칙을 설정할 수 있습니다
       'no-unused-vars': 'warn',
       'no-console': 'warn',
     },
   },
-<<<<<<< HEAD
-=======
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
->>>>>>> 8e2e46c (충돌 해결)
-=======
->>>>>>> 8c157eb9b616b8085218ea0e34c063dfe1678fc8
 ];
 
 export default eslintConfig;
